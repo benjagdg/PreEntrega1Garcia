2ba1a531943190306bc83fc678ee8dbaffefd366
Benjamin Garcia <benja.garcia.dg@gmail.com>
2023-12-20 16:26:02 -0300

    change card color

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap"
 const ItemDetail = ({productData}) => {
   return (
     <>
-      <Card>
+      <Card className="bg-light p-5">
         <Card.Body>
           <div className="row">
             <div className="col-5">
@@ -13,9 +13,9 @@ const ItemDetail = ({productData}) => {
               <h4>{productData.title}</h4>
               <p><b>{productData.brand} - {productData.model}</b></p>
               <p>{productData.description}</p>
-              <p>Valor: ${productData.price} | Stock: {productData.stock}</p>
+              <p>Valor: <b>${productData.price}</b> | Stock: {productData.stock}</p>
              
-              <button type="button" className="btn btn-success"><b>+</b> Agregar al Carrito</button>
+              <button type="button" className="btn w-100 btn-success"><b>+</b> Agregar al Carrito</button>
             </div>
           </div>
         </Card.Body>
